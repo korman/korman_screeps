d86e65dba0f9943b9b89628a101ce22cd9bad11b
Korman <korman@qq.com>
2025-11-04 02:23:43 +0800

    可以采集，但是交货后不继续了

--- src/game_logic/creep_logic.rs
+++ src/game_logic/creep_logic.rs
@@ -1,9 +1,14 @@
 use log::info;
+use log::{debug, warn};
+use screeps::HasPosition;
 use screeps::{
+    action_error_codes::*,
     constants::ResourceType,
+    enums::StructureObject,
     find,
     local::ObjectId,
     objects::{Creep, Source, StructureController},
+    prelude::*,
     SharedCreepProperties,
 };
 
@@ -37,8 +42,41 @@ pub fn run_creep(creep: &Creep, creep_targets: &mut HashMap<String, CreepTarget>
             info!("creep {} is working on {:?}", creep.name(), creep_target);
 
             match creep_target {
-                CreepTarget::Upgrade(controller_id) => {}
-                CreepTarget::Harvest(source_id) => {}
+                CreepTarget::Upgrade(controller_id) => {
+                    if creep.store().get_used_capacity(Some(ResourceType::Energy)) > 0 {
+                        if let Some(controller) = controller_id.resolve() {
+                            creep
+                                .upgrade_controller(&controller)
+                                .unwrap_or_else(|e| match e {
+                                    UpgradeControllerErrorCode::NotInRange => {
+                                        let _ = creep.move_to(&controller);
+                                    }
+                                    _ => {
+                                        warn!("can't upgrade controller {:?}", e);
+                                        entry.remove();
+                                    }
+                                });
+                        } else {
+                            entry.remove();
+                        }
+                    }
+                }
+                CreepTarget::Harvest(source_id) => {
+                    if creep.store().get_free_capacity(Some(ResourceType::Energy)) > 0 {
+                        if let Some(source) = source_id.resolve() {
+                            if creep.pos().is_near_to(source.pos()) {
+                                creep.harvest(&source).unwrap_or_else(|e| {
+                                    warn!("can't harvest source {:?}", e);
+                                    entry.remove();
+                                });
+                            } else {
+                                let _ = creep.move_to(&source);
+                            }
+                        }
+                    } else {
+                        entry.remove();
+                    }
+                }
             }
         }
         Entry::Vacant(entry) => {
@@ -47,7 +85,14 @@ pub fn run_creep(creep: &Creep, creep_targets: &mut HashMap<String, CreepTarget>
             let room = creep.room().expect("creep has no room");
 
             if creep.store().get_used_capacity(Some(ResourceType::Energy)) > 0 {
+                for structure in room.find(find::STRUCTURES, None).iter() {
+                    if let StructureObject::StructureController(controller) = structure {
+                        entry.insert(CreepTarget::Upgrade(controller.id()));
+                        break;
+                    }
+                }
             } else if let Some(source) = room.find(find::SOURCES_ACTIVE, None).first() {
+                entry.insert(CreepTarget::Harvest(source.id()));
             }
         }
     }
